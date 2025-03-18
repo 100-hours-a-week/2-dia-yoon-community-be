e173aa45a4a6128efef5adfacbec17ac7eb6150b
kingwhangzang <soyun011112@sookmyung.ac.kr>
2025-03-18 17:17:33 +0900

    Update Entity

--- src/main/java/com/example/community_spring/User/Entity/User.java
+++ src/main/java/com/example/community_spring/User/Entity/User.java
@@ -1,24 +1,45 @@
 package com.example.community_spring.User.Entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+// import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "User")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
+
     private String nickname;
+
+    @Column(unique = true)
     private String email;
+
     private String password;
     private String profileImage;
+
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
 
     @Override
     public String toString() {
