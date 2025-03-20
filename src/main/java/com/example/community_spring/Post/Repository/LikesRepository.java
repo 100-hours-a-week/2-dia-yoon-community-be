976302bc921abcca7ff50b2e8d59a049a14e6e59
kingwhangzang <soyun011112@sookmyung.ac.kr>
2025-03-20 15:35:57 +0900

    해당 게시글 전체 좋아요 삭제

--- src/main/java/com/example/community_spring/Post/Repository/LikesRepository.java
+++ src/main/java/com/example/community_spring/Post/Repository/LikesRepository.java
@@ -10,6 +10,8 @@ public class LikesRepository {
 
     private final JdbcTemplate jdbcTemplate;
 
+
+
     /**
      * 좋아요 추가
      */
@@ -42,5 +44,12 @@ public class LikesRepository {
         String sql = "SELECT COUNT(*) FROM Likes WHERE post_id = ?";
         Integer count = jdbcTemplate.queryForObject(sql, Integer.class, postId);
         return count != null ? count : 0;
+    }
+
+    // 게시물의 전체 좋아요 삭제
+    public void deleteAllByPostId(Long postId) {
+        String sql = "DELETE FROM Likes WHERE post_id = ?";
+        int deletedRows = jdbcTemplate.update(sql, postId);
+        System.out.println("삭제된 좋아요 수: " + deletedRows);
     }
 }
