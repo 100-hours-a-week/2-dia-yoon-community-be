64a008b9415a77584e64ae5979d5b42f87492d36
kingwhangzang <soyun011112@sookmyung.ac.kr>
2025-03-27 13:28:19 +0900

    비밀번호 규칙 수정

--- src/main/java/com/example/community_spring/User/DTO/request/SignupRequest.java
+++ src/main/java/com/example/community_spring/User/DTO/request/SignupRequest.java
@@ -4,14 +4,26 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class SignupRequest {
+    @NotBlank(message = "이메일은 필수입니다")
+    @Email(message = "유효한 이메일 형식이 아닙니다")
     private String email;
+
+    @NotBlank(message = "비밀번호는 필수입니다")
+    @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하여야 합니다")
     private String password;
+
+    @NotBlank(message = "닉네임은 필수입니다")
+    @Size(min = 2, max = 50, message = "닉네임은 2자 이상 50자 이하여야 합니다")
     private String nickname;
+
     private String profileImage;
 }
